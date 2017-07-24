32ce438251cbae70f418dbf6b1c72913f498eab9
Rob Sliwa <robjsliwa@gmail.com>
2017-07-24 14:26:10 -0400

    Updated test job to use Chronos SDK to provide progress update.

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
-let counter = 0
+const ChronosSDK = require('chronos-sdk');
 
-var startTime = Date.now();
-while ((Date.now() - startTime) < 60000) {
-  console.log(counter);
-  counter += 1
+const Config = {
+  "authManagerURL": "https://st-irisauth-wcdcc-002.poc.sys.comcast.net",
+  "chronosURL": "https://st-chronos-asb-001.poc.sys.comcast.net",
+  "appkey": "",
+  "appsecret": ""
 }
+
+let chronos = new ChronosSDK.Chronos(Config);
+const instanceID = chronos.getShortInstanceID()
+
+let counter = 0;
+let timeout;
+
+function *reportJobStatus(){
+  var startTime = Date.now();
+  while ((Date.now() - startTime) < 60000) {
+    yield chronos.updateJobStatus(instanceID, `Counter=${counter}`);
+  }
+}
+
+function runJob(generator) {
+  if (!generator) {
+    generator = reportJobStatus();
+  }
+
+  let j = generator.next();
+  if (j.value === undefined) {
+    return;
+  }
+  j.value.then(() => {
+    console.log(`Counter=${counter}`);
+    counter += 1;
+    runJob(generator);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
+
+runJob();
