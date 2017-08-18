ff01e3df5058ffb612b4057cde90705177983be7
Mark <mark_klobukov@comcast.com>
2017-08-18 13:07:59 -0400

    Added a test for getArgs API after the counter is done

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 const ChronosSDK = require('chronos-sdk');
 
+// const Config = {
+//   "authManagerURL": "https://st-irisauth-wcdcc-002.poc.sys.comcast.net",
+//   "chronosURL": "https://st-chronos-asb-001.poc.sys.comcast.net",
+//   "appkey": "",
+//   "appsecret": ""
+// }
+
 const Config = {
-  "authManagerURL": "https://st-irisauth-wcdcc-002.poc.sys.comcast.net",
-  "chronosURL": "https://st-chronos-asb-001.poc.sys.comcast.net",
+  "authManagerURL": "http://localhost:4655",
+  "chronosURL": "http://localhost:8080",
   "appkey": "",
   "appsecret": ""
 }
 
 let chronos = new ChronosSDK.Chronos(Config);
-const instanceID = chronos.getShortInstanceID()
-
+//const instanceID = chronos.getShortInstanceID()
+const instanceID = "localtest"
+console.log("instanceID: ", instanceID)
 let counter = 0;
 let timeout;
 
 function *reportJobStatus(){
   var startTime = Date.now();
-  while ((Date.now() - startTime) < 60000) {
+  while ((Date.now() - startTime) < 2000) {
     yield chronos.updateJobStatus(instanceID, `Counter=${counter}`);
   }
 }
@@ -27,6 +35,16 @@ function runJob(generator) {
 
   let j = generator.next();
   if (j.value === undefined) {
+    //get arguments and display them before exiting
+    //replace this test ID with any instanceID from your mongoDB entries
+    let testInstanceID = "e747d9cd4cfb713a9f912405c4f02770b610aecb61918e0bae53c32195d47425"
+    chronos.getJobArgs(testInstanceID)
+    .then(response => {
+      console.log("Got arguments: ", response)
+    })
+    .catch(error => {
+      console.log("Error getting arguments: ", error)
+    })
     return;
   }
   j.value.then(() => {
@@ -37,6 +55,7 @@ function runJob(generator) {
   .catch(err => {
     console.log(err);
   });
+
 }
 
 runJob();
